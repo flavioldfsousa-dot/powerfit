72b62cb7b422c673564702bd7ae7919134b53714
AnnaGabrielly <annagabrielly@MacBook-Air-de-AnnaGabrielly.local>
2026-07-03 08:13:11 +0200

    Fix missing TextView import in PerfilFragment

--- app/src/main/java/com/powerfit/app/ui/PerfilFragment.kt
+++ app/src/main/java/com/powerfit/app/ui/PerfilFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.powerfit.app.R
